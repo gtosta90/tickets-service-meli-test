182e3ad1ee3552e21c4326bc5bb64c8330edfa39
Gabriel Tosta <gabriel.tosta@cliqx.com.br>
2024-08-20 15:17:09 -0300

    Add postgres config, dockerize application and set environments configs

--- manage.py
+++ manage.py
@@ -2,12 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
+from pathlib import Path
 
 def main():
     """Run administrative tasks."""
     sys.path.append("src")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+    dotenv_path = Path(__file__).resolve().parent / '.env'
+    load_dotenv(dotenv_path=dotenv_path)
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +79,25 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tickets_db',
+#         'USER': 'docker_user',
+#         'PASSWORD': 'docker_user',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
+print(f"{BASE_DIR}/{os.environ.get("DB_NAME")}")
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': f"{BASE_DIR}/{os.environ.get("DB_NAME")}",
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
